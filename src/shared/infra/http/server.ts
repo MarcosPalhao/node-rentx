c2d6e5e8a4d5a874e47d111ab9e223089ef3387b
Marcos Vinicius <marcos.palhao18@gmail.com>
2022-10-21 10:59:01 -0300

    feat: implementation to consult available cars

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,12 +1,13 @@
 import express, { NextFunction, Request, Response } from 'express';
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
-import "../typeorm";
-import "@shared/container";
+import createConnection from "@shared/infra/typeorm";
+import "@shared/container"; 
 import { router } from './routes';
 import swaggerFile from '../../../swagger.json';
 import { AppError } from '@shared/errors/AppError';
 
+createConnection();
 const app = express();
 
 app.use(express.json());
